622ab8f4481f446c3f5df0c5ebf3704ed60dac97
tomcat0312 <52883708+tomcat0312@users.noreply.github.com>
2024-07-30 20:21:16 +0200

    Update __init__.py

--- components/esp32_ble_presense/__init__.py
+++ components/esp32_ble_presense/__init__.py
@@ -27,8 +27,8 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     cg.add_library(
-        name="NimBLE-Arduino",
-        version="1.4.2",
+        name="esp-nimble-cpp",
+        version="1.4.1",
     )
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
